cefdd51f72c3e87483c63be05c373c16c4dd4cab
Aaron Noe <aarminoe@gmail.com>
2022-07-29 19:29:53 -0500

    commit tree 885884413a074cdc75e5507800271587531f180c

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json: user, status: :created
+            session[:user_id] = user.id
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def show 
+        user = User.find_by(id:session[:user_id])
+        if user
+            render json: user, status: :created
+        else
+            render json: { error: 'Not authorized' }, status: :unauthorized
+        end
+    end
+
+    private 
+
+    def user_params 
+        params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+    end
+end
